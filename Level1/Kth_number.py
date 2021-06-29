2d40a298ebc4fe8e092dcf0ea02cb86df773fc03
yeye921 <77521756+yeye921@users.noreply.github.com>
2021-06-29 23:14:15 +0900

    K번째수

--- Level1/Kth_number.py
+++ Level1/Kth_number.py
@@ -2,7 +2,7 @@
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
-# i,j,k 한번에 입력할 수도 있음
+# i,j,k 한번에 입력할 수도 있음 
 def solution(array, commands):
     answer = []
     for command in commands:
