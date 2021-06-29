b8f82f02ee023aeeacc43814fe33f411132e5408
yeye921 <77521756+yeye921@users.noreply.github.com>
2021-06-29 23:13:42 +0900

    Update Kth_number.py

--- Level1/Kth_number.py
+++ Level1/Kth_number.py
@@ -1,8 +1,8 @@
-// 다른풀이(map,람다함수 공부하기)
+# 다른풀이(map,람다함수 공부하기)
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
-// i,j,k 한번에 입력할 수도 있음
+# i,j,k 한번에 입력할 수도 있음
 def solution(array, commands):
     answer = []
     for command in commands:
