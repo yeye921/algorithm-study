c0e518098fe77e913edfdf4331ecd497dae63271
yeye921 <77521756+yeye921@users.noreply.github.com>
2021-06-30 12:10:25 +0900

    K번째 수

--- Level1/Kth_number.py
+++ Level1/Kth_number.py
@@ -1,3 +1,14 @@
+# commands 어떻게 처리하는 지가 관건
+#  2차원 배열의 반복문 처리(한 행씩 처리)
+# list의 n번째 => list[n-1] 
+def solution(array, commands):
+  answer = []
+  for i in range(len(commands)):
+        arr_list = array[commands[i][0]-1:commands[i][1]]
+        arr_list.sort()
+        answer.append(arr_list[commands[i][2]-1])
+  return answer
+
 # 다른풀이(map,람다함수 공부하기)
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
