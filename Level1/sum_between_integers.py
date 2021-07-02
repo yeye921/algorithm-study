d96c6cc09a3b5ef932ca2898f410369de8093016
yewon921 <yewon921@ajou.ac.kr>
2021-07-02 22:50:57 +0900

    두 정수 사이의 합

--- Level1/sum_between_integers.py
+++ Level1/sum_between_integers.py
@@ -1,12 +1,21 @@
+# a,b중 작은 수 구해서, 그 수를 1씩 증가시키면서 더하는 방식
 def solution(a, b):
-    answer = 0
-    if a == b:
-        return a
-    elif a > b:
-        for i in range(b, a+1):
-            answer += i        
-        return answer
+    result = 0
+    if a>b:
+        x = b
     else:
-        for i in range(a, b+1):
-            answer += i        
-        return answer
+        x = a
+    for _ in range(abs(a-b)+1): # 단순 횟수 반복
+        result += x
+        x += 1
+    return result
+
+
+# 다른 풀이
+def solution(a,b):
+    if a > b: a, b = b, a
+    return sum(range(a,b+1))
+
+# 최적화 코드(시간복잡도:O(1))
+def solution(a,b):
+    return (abs(a-b)+1)*(a+b)//2   # 수열 공식
