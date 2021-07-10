f852d7b7aa8d8c98ed6471e6c22acc7c63025ab3
yewon921 <yewon921@ajou.ac.kr>
2021-07-10 22:38:04 +0900

    2016년

---
+++ Level1/2016_year.py
@@ -0,0 +1,13 @@
+def getDayName(a,b):
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return days[(sum(months[:a-1])+b-1)%7]
+
+
+
+# 다른 풀이
+import datetime
+
+def getDayName(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
