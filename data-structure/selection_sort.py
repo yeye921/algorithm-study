eae388963e45f39857f25c09c0351f2dcb18c2a8
yewon921 <yewon921@ajou.ac.kr>
2021-07-11 23:28:22 +0900

    선택 정렬

---
+++ data-structure/selection_sort.py
@@ -0,0 +1,17 @@
+# 매번 가장 작은 것을 선택하는 알고리즘 
+# 가장 작은 데이터를 앞으로 보내는 과정을 N-1번 반복하면 정렬이 완료됨
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min = i   # 가장 작은 원소의 인덱스 
+        for j in range(i+1,len(arr)):
+            if arr[min] > arr[j]:
+                min = j
+
+        arr[i],arr[min] = arr[min],arr[i]
+
+
+array = [7,5,9,0,3,1,6,2,4,8]
+selection_sort(array)
+print(array)
