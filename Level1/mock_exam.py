06da8224a1d5f10229426ae87db4944f91c8243a
yewon921 <yewon921@ajou.ac.kr>
2021-07-12 23:39:42 +0900

    모의고사

--- Level1/mock_exam.py
+++ Level1/mock_exam.py
@@ -2,9 +2,10 @@ def solution(answers):
     A = [1,2,3,4,5]
     B = [2,1,2,3,2,4,2,5]
     C = [3,3,1,1,2,2,4,4,5,5]
-    cnt_A,cnt_B.cnt_C = 0
+    cnt_A,cnt_B,cnt_C = 0,0,0
 
     for i in range(len(answers)):
+        # 반복되는 배열의 인덱스 표현 방식
         a = i%5
         b = i%8
         c = i%10
@@ -16,14 +17,38 @@ def solution(answers):
         if C[c] == answers[i]:
             cnt_C += 1
 
-    max = max(cnt_A,cnt_B,cnt_C)
+    k = max(cnt_A,cnt_B,cnt_C)
     answer = []
 
-    if max == cnt_A:
+    if k == cnt_A:
         answer.append(1)
-    if max == cnt_B:
+    if k == cnt_B:
         answer.append(2)
-    if max == cnt_C:
+    if k == cnt_C:
         answer.append(3)
 
     return answer
+
+
+
+# 다른 풀이
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for i in range(len(answers)):
+        if answers[i] == pattern1[i%len(pattern1)]: # 이전 코드와 동일한 방식 
+            score[0] += 1
+        if answers[i] == pattern2[i%len(pattern2)]:
+            score[1] += 1
+        if answers[i] == pattern3[i%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
