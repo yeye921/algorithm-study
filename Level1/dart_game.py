72991d63e91ef87f00a614882b83c3ecc17cafcb
yewon921 <yewon921@ajou.ac.kr>
2021-07-13 23:02:06 +0900

    다트 게임

--- Level1/dart_game.py
+++ Level1/dart_game.py
@@ -1,28 +1,31 @@
-def solution(dartResult):
-    n = ''
+# 점수 & 보너스 처리한 다음에 배열에 넣음, 옵션은 배열안의 원소에 처리
+
+def solution(dart):
+    n = ''    # n이 문자열이어야 10일 경우 +로 처리가능해짐
     score = []
-    for i in dartResult:
-        if i.isnumeric():
+    for i in dart:
+        if i.isdigit():
             n += i
         elif i == 'S':
             n = int(n)**1
             score.append(n)
             n = ''
         elif i == 'D':
-            n = int(n)**2
+            n == int(n)**2
             score.append(n)
             n = ''
         elif i == 'T':
-            n = int(n)**3
+            n == int(n)**3
             score.append(n)
             n = ''
-        elif i == '*':
-            if len(score) > 1:
-                score[-2] = score[-2] * 2
-                score[-1] = score[-1] * 2
-            else:
-                score[-1] = score[-1] * 2
         elif i == '#':
-            score[-1] = score[-1] * -1
-        
+            score[-1] *= -1
+        elif i == '*':
+            if len(score)>1:
+                score[-1] *= 2
+                score[-2] *= 2
+            else:
+                score[-1] *= 2
+
     return sum(score)
+        
