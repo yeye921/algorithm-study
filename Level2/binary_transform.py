6655e8b68a435a68ca0261edd28caa9ff4ccd81e
yewon921 <yewon921@ajou.ac.kr>
2021-07-22 23:32:56 +0900

    이진 변환 반복하기

---
+++ Level2/binary_transform.py
@@ -0,0 +1,23 @@
+# s에서 0을 제외한 길이를 2진법으로 나타내기(s가 1이 될 때까지 반복)
+# [이진변환 횟수, 제거된 0의 개수]
+def solution(s):
+    cnt_z = 0
+    cnt_b = 0
+    
+    while s != "1":
+        cnt_z += s.count("0")
+        s = s.replace("0","")
+        s = binary(len(s))
+        cnt_b += 1
+    return [cnt_b,cnt_z]
+        
+def binary(l):
+    r = ''
+    while l != 1:
+        r += str(l % 2)
+        l = l // 2
+    r = r + '1'
+    r = r[::-1]
+    return r
+        
+    
