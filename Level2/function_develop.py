62c077cd320820d01a2c1c827e99a709d6035f55
yewon921 <yewon921@ajou.ac.kr>
2021-08-04 23:23:21 +0900

    기능 개발

---
+++ Level2/function_develop.py
@@ -0,0 +1,28 @@
+def solution(progresses, speeds):
+    print(progresses)
+    print(speeds)
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+# 다른 풀이
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s): 
+            Q.append([-((p-100)//s),1]) # 필요한 작업일수 계산
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
