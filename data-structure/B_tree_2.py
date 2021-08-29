f5ad47e5f3648c40e25cf012a1de633ca1e5c936
yewon921 <yewon921@ajou.ac.kr>
2021-08-29 23:27:00 +0900

    이진 트리_2

---
+++ data-structure/B_tree_2.py
@@ -0,0 +1,73 @@
+# 노드 생성과 삽입
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+        
+class BinarySearchTree(Node):
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+
+# 노드 탐색
+    def find(self, key):
+        return self._find_value(self.root, key)
+
+    def _find_value(self, root, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)
+
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+    
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+
+    def _delete_value(self, node, key):
+        if node is None:
+            return node, False
+
+        deleted = False
+        # 해당 노드가 삭제할 노드일 경우
+        if key == node.data:
+            deleted = True
+            # 삭제할 노드가 자식이 두개일 경우
+            if node.left and node.right:
+                # 오른쪽 서브 트리에서 가장 왼쪽에 있는 노드를 찾고 교체
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            # 자식 노드가 하나일 경우 해당 노드와 교체
+            elif node.left or node.right:
+                node = node.left or node.right
+            # 자식 노드가 없을 경우 그냥 삭제
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+        else:
+            node.right, deleted = self._delete_value(node.right, key)
+        return node, deleted
+
