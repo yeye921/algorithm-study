4828d47f7492b867ecb903b15546f24333b2c500
yewon921 <yewon921@ajou.ac.kr>
2021-09-02 23:42:49 +0900

    문자열 폭발

---
+++ baeckjoon/9935.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    s = input().strip()
+    p = input().strip()
+
+    stack = []
+    for i in range(len(s)):
+        stack.append(s[i])
+
+        if len(stack) >= len(p):
+            tmp = "".join(stack[-len(p) :])
+            if tmp == p:
+                cnt = 0
+                while cnt < len(p):
+                    stack.pop()
+                    cnt += 1
+
+    if len(stack) == 0:
+        print("FRULA")
+    else:
+        print("".join(stack))
